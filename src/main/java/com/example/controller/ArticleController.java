c87e630247df6ebe556a2cbced3a25741c60e70e
SuzukiKumpei <suzuki.kumpei@gmail.com>
2020-02-13 16:48:22 +0900

    HTMLファイルの完了

---
+++ src/main/java/com/example/controller/ArticleController.java
@@ -0,0 +1,67 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Article;
+import com.example.form.ArticleForm;
+import com.example.repository.ArticleRepository;
+
+/**
+ * 掲示板に関する情報を処理するコントローラ.
+ * 
+ * @author suzukikunpei
+ *
+ */
+@Controller
+@RequestMapping("/bbs")
+public class ArticleController {
+
+	/**
+	 * フォームのインスタンス化
+	 * 
+	 * @return 記事のフォーム
+	 */
+	@ModelAttribute
+	public ArticleForm form() {
+		return new ArticleForm();
+	}
+
+	@Autowired
+	private ArticleRepository articleRepository;
+
+	/**
+	 * 投稿内容を表示させる初期画面.
+	 * 
+	 * @param articleForm 記事のフォーム
+	 * @param model       リクエストスコープ
+	 * @return 投稿内容
+	 */
+	@RequestMapping("")
+	public String index(ArticleForm articleForm, Model model) {
+		Article article = new Article();
+		article.setName(articleForm.getName());
+		article.setContent(articleForm.getContent());
+		model.addAttribute("articleList", articleRepository.findAll());
+		return "bbs";
+	}
+
+	/**
+	 * 投稿を行う.
+	 * 
+	 * @param articleForm
+	 * @return 投稿者と投稿内容
+	 */
+	@RequestMapping("/insert")
+	public String insertArticle(ArticleForm articleForm) {
+		Article article = new Article();
+		article.setName(articleForm.getName());
+		article.setContent(articleForm.getContent());
+		articleRepository.insert(article);
+		return "redirect:/bbs";
+	}
+
+}
--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -10,9 +10,11 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Article;
+import com.example.form.ArticleForm;
 
 /**
  * 記事に関する情報を処理するリポジトリ.
+ * 
  * @author suzukikunpei
  *
  */
@@ -20,47 +22,57 @@ import com.example.domain.Article;
 public class ArticleRepository {
 	
 	@Autowired
-	private NamedParameterJdbcTemplate template;
+	public ArticleForm form() {
+		return new ArticleForm();
+	}
 	
-	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs,i) ->{
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	private static final RowMapper<Article> ARTICLE_ROW_MAPPER = (rs, i) -> {
 		Article article = new Article();
 		article.setId(rs.getInt("id"));
 		article.setName(rs.getString("name"));
 		article.setContent(rs.getString("content"));
 		return article;
 	};
-	
+
 	/**
 	 * 記事に関する情報の全件検索を行う.
+	 * 
 	 * @return 全件検索結果
 	 */
-	public List<Article> findAll(){
-		String sql = "SELECT id,name,content FROM articles ORDER BY id";
+	public List<Article> findAll() {
+		String sql = "SELECT id,name,content FROM articles ORDER BY id DESC";
 		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
 		return articleList;
 	}
-	
+
 	/**
 	 * 記事の内容を入力する.
+	 * 
 	 * @param article 記事
-	 * @param name　投稿者名
-	 * @param content　記事の内容
+	 * @param name    投稿者名
+	 * @param content 記事の内容
+	 * @return  追加する投稿内容
 	 */
-	public void insert(Article article,String name,String content) {
-		String sql = "INSERT INTO articles(name,content) VALUES(:name,:content)";
-		SqlParameterSource param = new MapSqlParameterSource().addValue("name", name).addValue("content",content);
-		template.queryForObject(sql, param, ARTICLE_ROW_MAPPER);
+	public void insert(Article article) {
+		
+		String sql = "INSERT INTO articles (name, content) VALUES (:name, :content)";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("name",article.getName()).addValue("content",article.getContent());
+		 template.update(sql, param);
 	}
-	
+
 	/**
 	 * 入力したIDの記事を削除.
+	 * 
 	 * @param id 記事ID
 	 */
 	public void deleteById(int id) {
 		String sql = "DELETE id,name,content FROM articles WHERE id = :id";
-		SqlParameterSource param = new MapSqlParameterSource().addValue("id",id);
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 		template.queryForObject(sql, param, ARTICLE_ROW_MAPPER);
 	}
-	
 
 }
